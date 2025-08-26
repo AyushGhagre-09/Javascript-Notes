9527c7e9ae2fe6a3f884319e5471ce91e65dd22a
Ayush Ghagre <147733805+AyushGhagre-09@users.noreply.github.com>
2025-08-26 13:42:49 +0530

    Update app.js

--- part1/app.js
+++ part1/app.js
@@ -1,23 +1,21 @@
-// function sanitize(x) {
-//     if (Number.isNaN(x)) {
-//       return NaN;
-//     }
-//     return x;
-//   }
-  
-//   console.log(sanitize("1"));
-//   // Expected output: "1"
-  
-//   console.log(sanitize("NotANumber"));
-  // Expected output: NaN
+// REPL(Read-Evaluate-Print-Loop).
+// ctrl +l (clear screen).
 
-  isNaN(123);   // false, because 123 is a valid number
-  isNaN('123'); // false, because '123' can be converted to a number
-  isNaN('abc'); // true, because 'abc' cannot be converted to a number
-  isNaN(NaN);   // true, because NaN is not a number
-  isNaN(undefined); // true, because undefined is not a valid number
-  
-  Number.isNaN(123);    // false
-Number.isNaN('123');  // false
-Number.isNaN('abc');  // false
-Number.isNaN(NaN);    // true
+//Variable
+// A variable is simply the name of  storage location.
+
+// Data Type
+// 1)Number
+// 2)Boolean
+// 3)String
+// 4)undefined
+// 5)null
+// 6)BigInt
+// 7)Symbol
+
+// typeof
+// typeof(17.6) Number
+
+// NAN-> Not a number
+
+
